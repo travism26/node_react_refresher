dc2f60720193ca5fa3c83285a5ea1f42e2ab0cc5
travis martin <travismartin266@gmail.com>
2023-10-15 13:07:52 -0300

    add query service

--- comments/index.js
+++ comments/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 // curl http://localhost:4001/posts/123/comments
 app.get('/posts/:id/comments', (req, res) => {
+    console.log('Recieved get request');
     res.send(commentsByPostId[req.params.id] || []);
 });
 
@@ -18,7 +22,8 @@ app.get('/posts/:id/comments', (req, res) => {
 --data '{ "content": "hello world" }' \
 http://localhost:4001/posts/123/comments
  */
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
+    console.log('Recieved POST request');
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -27,7 +32,21 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+
     res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
 });
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 // curl http://localhost:4000/posts
 app.get('/posts', (req, res) => {
+    console.log('Recieved get request');
     res.send(posts);
 });
 
@@ -18,14 +22,28 @@ curl --header "Content-Type: application/json" \
 --data '{ "title": "hello" }' \
 http://localhost:4000/posts
  */
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
+    console.log('Recieved post request' + title);
     posts[id] = {
         id,
         title
     };
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id,
+            title
+        }
+    });
+
     res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
 });
 
 app.listen(4000, () => {
